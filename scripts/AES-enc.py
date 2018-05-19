7ad4d85f2efd7549d25d40a6fa350c72e023c83b
Shorouq Mohamed Saleh <Shiro@Shorouq-Saleh.local>
2018-05-20 01:26:13 +0400

    aes form test 2

--- scripts/AES-enc.py
+++ scripts/AES-enc.py
@@ -1,4 +1,5 @@
 import cgi
 form = cgi.FieldStorage()
 searchterm =  form.getvalue('searchbox')
-document.getElementById('display').innerHTML = searchterm;
+print searchterm
+#document.getElementById('display').innerHTML = searchterm;
