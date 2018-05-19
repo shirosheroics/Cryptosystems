9bf99f70f9fed420b2a70d4f847ddedffc9bc3ad
Shorouq Mohamed Saleh <Shiro@Shorouq-Saleh.local>
2018-05-20 02:10:37 +0400

    script edit

--- scripts/AES-enc.py
+++ scripts/AES-enc.py
@@ -1,5 +1,11 @@
 import cgi
 form = cgi.FieldStorage()
 searchterm =  form.getvalue('searchbox')
-print searchterm
+print '''<html>
+<head><title>My first Python CGI app</title></head>
+<body>
+<p>Hello, 'world'!</p>
+</body>
+</html>'''
+#print searchterm
 #document.getElementById('display').innerHTML = searchterm;
